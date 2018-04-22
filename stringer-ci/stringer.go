74d210aca0cf855c6798049248341fcf16f69de2
Joshua Haupt <joshua@hauptj.com>
2018-04-22 02:54:17 -0500

    debug CI test - added main()

--- stringer-ci/stringer.go
+++ stringer-ci/stringer.go
@@ -40,3 +40,13 @@ func format_map(hosts map[string]IPAddr) [2]string {
 	}
 	return hostnames
 }
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
